f7141d1141bd5806b25e672fd8bac1983bed4ae2
qsunki <ajsthfldu@outlook.com>
2023-11-13 15:47:54 +0900

    feat: 프로젝트 셋팅

---
+++ src/main/java/org/tierup/grading/TierUpBackendGradingApplication.java
@@ -0,0 +1,13 @@
+package org.tierup.grading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierUpBackendGradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierUpBackendGradingApplication.class, args);
+    }
+
+}
